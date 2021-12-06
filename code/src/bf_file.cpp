1a0381278aed67c446f04c9f7a2f03c80f519a30
DimGj <91105160+DimGj@users.noreply.github.com>
2021-12-06 22:39:16 +0200

    Update bf_file.cpp

--- code/src/bf_file.cpp
+++ code/src/bf_file.cpp
@@ -3,10 +3,8 @@
 #include "bf.h"
 #include "hash_file.h"
 
-using nanespace std;
-
 typedef struct BF_Block {
-    Record *Array;
+    Record * Array;
     bool DirtyStatus;
     bool Pinned;
 } BF_Block;
@@ -14,20 +12,24 @@ typedef struct BF_Block {
 void BF_Block_Init(BF_Block **block) //When this function is called,the block arg will be a pointer to the block,hence the double pointer block variable
 {
     *block = new BF_Block;
-    (*block)->Array = new Record[10]; //the max bytes stored in a block are 512 and each record is 49
-    (*block)->DirtyStatus = false; //Each new block isn't dirty
+    (*block)->Array = new Record[10];   //the max bytes stored in a block are 512 and each record is 49
+    (*block)->DirtyStatus = false;      //Each new block isn't dirty
     (*block)->Pinned = false;
+    cout << "A new block has been created!" << endl;
 } 
 
 void BF_Block_Destroy(BF_Block **block)
 {
     delete[] (*block)->Array;               //delete first the array of records and then the whole block
     delete *block;
+    cout << "The block has been successfully deleted" << endl;
+    return ;
 }
 
 void BF_Block_SetDirty(BF_Block *block)
 {
-    block->DirtyStatus = true;
+    block->DirtyStatus = true;              //block was marked as dirty
+    return ;
 }
 
 char* BF_Block_GetData(const BF_Block *block)
@@ -46,6 +48,7 @@ int main()
 {
     BF_Block *A;
     BF_Block_Init(&A);
+    //A->Array[0].city="Athens";
     BF_Block_SetDirty(A);
     BF_Block_GetData(A);
     BF_Block_Destroy(&A);
