c81523425ffe851fcd192e4863e8e83416835506
natedrake7 <92892346+natedrake7@users.noreply.github.com>
2021-12-12 22:49:22 +0200

    Add files via upload

--- hash_file.cpp
+++ hash_file.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -14,25 +17,30 @@ using namespace std;
     return HT_ERROR;        \
   }                         \
 }
-typedef struct FileArray
-{
-  string Name;
-  string Type;
-  string GeneralInfo;
-}FileArray;
 
-FileArray* Array;
+typedef struct FileList
+{
+  int ID;
+  string Name;
+
+}FileList;
+
+FileList* Array;
 
 HT_ErrorCode HT_Init() {
-  BF_Block *Block;
-  Array = new FileArray[20];
+  BF_Block* Block;
   BF_Block_Init(&Block);
+  CALL_BF(BF_Init(LRU));
+  Array = new FileList[20];
   return HT_OK;
 }
 
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
+  int file_desc;BF_Block* Block;
   CALL_BF(BF_CreateFile(filename));
-  BF_Init(LRU);
+  CALL_BF(BF_OpenFile(filename,&file_desc)); //epistrfei to id sto file desc
+
+  CALL_BF(BF_AllocateBlock(file_desc,Block)); //meta thelei unpin,epistrfei to block sth metavlith block
   int StartingDepth = depth;
   //what else needs to happen here?
   //Where does depth go?
@@ -40,7 +48,14 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
 }
 
 HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
+  BF_Block* Block;
+  char* ch;
   CALL_BF(BF_OpenFile(fileName,indexDesc));
+  if(indexDesc < 0)
+    return HT_ERROR;
+  else{
+    
+  }
   //insert code here
   return HT_OK;
 }
